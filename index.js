95d883afb4dc188848561fda40a857f529f29a43
Vlad <vl.tansky@gmail.com>
2020-05-03 14:48:27 +0300

    get latest zip from github api

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
 const request = require('request');//TODO: use got or smth else
 const unzipper = require('unzipper');
+const path = require("path");
 
-const url = 'https://github.com/h5bp/html5-boilerplate/releases/latest/download/html5-boilerplate_v7.3.0.zip';
+const args = process.argv.slice(2);
 
 (async () => {
-  unzipper.Open.url(request, url).then(d=>d.extract({path: ''}));
+    console.log('downloading...');
+    const release = request({
+        url:'https://api.github.com/repos/h5bp/html5-boilerplate/releases/latest',
+        headers: {
+          'User-Agent': 'request'
+        }
+    }, async(error, response, body) =>{
+        const data = JSON.parse(body);
+        const zipURL = data.assets[0].browser_download_url;
+        if(!zipURL){
+            throw new Error('Unexpected error')
+        }
+        const extractPath = path.resolve(path.normalize(args[0] || ''));
+        await unzipper.Open.url(request, zipURL).then(d=>d.extract({path: extractPath}));
+        console.log('done');
+    });
 })();
