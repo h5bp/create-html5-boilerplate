71cf35b23f818793acffd122b05d2f28e26e2371
Vlad <vl.tansky@gmail.com>
2020-05-13 20:32:42 +0300

    refactor: pacote

--- index.js
+++ index.js
@@ -4,36 +4,27 @@ const argv = require('yargs-parser')(process.argv.slice(2), {alias:{release: ['r
 const path = require("path");
 const chalk = require('chalk');
 const ora = require("ora");
-const pacote = require('pacote');
+const { extract } = require('pacote');
 const fs = require("fs-extra");
 const os = require('os');
 const packageName = 'html5-boilerplate';
 
-const extract = async(tempDir, version = '') => {
-    return new Promise((resolve, reject) => {
-        pacote.extract(packageName+'@'+version, tempDir, {})
-        .then(res=>resolve(res))
-        .catch(err=>{
-            reject(err);
-        });
-    });
-}
-
 (async () => {
-    const version = argv['release'];
+    const version = argv['release'] || '';
     const targetDir = path.resolve(argv['_'][0] || './');
     const spinner = ora(`Downloading ${packageName} ${version ? 'version '+version : 'latest version'} to ${targetDir}`).start();
     const tempDir = os.tmpdir()+`/${packageName}-staging`;
     await fs.ensureDir(tempDir);
     try{
-        const { from: nameWithVersion } = await extract(tempDir, version);//{from, resolved, integrity}
+        const { from: nameWithVersion } = await extract(packageName+'@'+version, tempDir, {});
 	    spinner.text = `${nameWithVersion} copied to ${targetDir}. Have fun!`;
     }catch(err){
         if(err.code === 'ETARGET' && err.type === 'tag'){
-            console.log(chalk.red(`version '${err.wanted}' not found in npm registry\navaialable versions:`));
+            spinner.fail(chalk.red(`version '${err.wanted}' not found in npm registry\navaialable versions:`));
             console.log(err.versions.reverse().join(' | '));
         }else{
-            console.log(err);
+            spinner.fail('Unexpected error');
+            console.error(err);
         }
         process.exit(0);
     }
