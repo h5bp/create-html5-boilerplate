b1c4194545fdd2a2e36885b0677d0e22d69b5509
Vlad <vl.tansky@gmail.com>
2020-05-15 13:39:31 +0300

    tests: add blank version & 'latest' & blank output folder

--- tests/test.js
+++ tests/test.js
@@ -1,85 +1,124 @@
-const fs = require('fs-extra');
-const cli = require("../cli");;
-const os = require('os');
-const packageName = 'html5-boilerplate';
-const tempDir = os.tmpdir()+`/${packageName}-staging`;
-
+const fs = require("fs-extra");
+const cli = require("../cli");
+const os = require("os");
+const packageName = "html5-boilerplate";
+const tempDir = os.tmpdir() + `/${packageName}-staging`;
+const defaultDir = "./out/default_dir";
 
 // TODO: fetch all versions:
 // const { packument } = require('pacote');
 // const versions = await packument(packageName);
 // cases = ['', 'latest', '-r=7.3.0', ...Object.keys(versions)];
 const all_versions = [
-  '0.0.1',
-  '5.3.0',
-  '6.0.0',
-  '6.0.1',
-  '6.1.0',
-  '7.0.0',
-  '7.0.1',
-  '7.1.0',
-  '7.2.0',
-  '7.3.0'
+  "0.0.1",
+  "5.3.0",
+  "6.0.0",
+  "6.0.1",
+  "6.1.0",
+  "7.0.0",
+  "7.0.1",
+  "7.1.0",
+  "7.2.0",
+  "7.3.0",
 ];
-const cases = ['-r=7.2.0','-r=v7.2.0','-r=v7.2', '--release=7.3.0', ...all_versions.map(v=>'-r='+v)];
+const cases = [
+  null,
+  "-r=latest",
+  "-r=7.2.0",
+  "-r=v7.2.0",
+  "-r=v7.2",
+  "--release=7.3.0",
+  ...all_versions.map((v) => "-r=" + v),
+];
+const versionFolder = (version = null) =>
+  version ? `./out/${version}` : defaultDir;
 
-const runCli = async(version) => {//TODO: blank output
-  process.argv.push(`./out/${version}`);
-  if(version){
-    process.argv.push(version)
+const runCli = async (version = null) => {
+  let prevCwd;
+  if (version) {
+    process.argv.push(version);
+    process.argv.push(versionFolder(version));
+  } else {
+    await fs.ensureDir(defaultDir);
+    prevCwd = process.cwd();
+    process.chdir(defaultDir);
   }
   await cli();
-  process.argv = process.argv.filter(v=>v!==version && v !== `./out/${version}`);//revert process args
-}
-describe.each(cases)(
-    "Downloading %s",
-    (version) => {
-      beforeAll(async () => {
-        await runCli(version);
-      });
-      afterAll(async () => {
-        await fs.remove(`./out/${version}`);
-      });
-
-      if(version){
-        test(`version ${version}`, async() => {
-            const cssContent = await fs.readFile(`./out/${version}/css/main.css`, 'utf-8');
-            let versionClear = version.replace(/(-r=|--release=|v)/gi, '');
-            if(versionClear === '7.0.0'){ versionClear = '6.1.0'}
-            if(versionClear === '0.0.1'){ versionClear = '5.3.0'}
-            expect(cssContent.indexOf(`HTML5 Boilerplate v${versionClear}`) > -1).toBe(true);
-        });
-      }
-
-      test('./out directory exists', async() => {
-          const outDirExists = await fs.exists(`./out/${version}`);
-          expect(outDirExists).toBe(true);
-      });
-      test('./out directory have files', async() => {
-          const dirContents = await fs.readdir(`./out/${version}`);
-          expect(dirContents.length).toBeGreaterThanOrEqual(5);
-      });
-      test('./out directory specific files', async() => {
-          const dirContents = await fs.readdir(`./out/${version}`);
-          const check = ['index.html', 'robots.txt','tile.png', 'css', 'js', 'img'].filter(v=>dirContents.indexOf(v)=== -1);
-          expect(check.length === 0).toBe(true);
-      });
-      test('Temp dir removed', async() => {
-          const tempDirExists = await fs.exists(tempDir);
-          expect(tempDirExists).toBe(false);
-      });
-    }
-  );
-
-  describe("Errors", () => {
-    test("wrong version 6..2.3", async()=>{
-      const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {});
-      const version = '-r=6..2.3';
-      
-      await runCli(version);
-      
-      await fs.remove(`./out/${version}`);
-      expect(mockExit).toHaveBeenCalledWith(1);
-      mockExit.mockRestore();
-    })
+  if (version) {
+    process.argv = process.argv.filter(
+      (v) => v !== version && v !== versionFolder(version)
+    ); //revert process args
+  } else {
+    process.chdir(prevCwd);//revert process current dir
+  }
+};
+describe.each(cases)("Downloading %s", (version) => {
+  beforeAll(async () => {
+    await runCli(version);
   });
+  afterAll(async () => {
+    await fs.remove(versionFolder(version));
+  });
+
+  if (version && version != "-r=latest") {
+    // if we will fetch all versions from npm registry we will be able to check latest
+    // for now we will skip this test for 'latest' version
+    test(`version ${version}`, async () => {
+      const cssContent = await fs.readFile(
+        `./out/${version}/css/main.css`,
+        "utf-8"
+      );
+      let versionClear = version.replace(/(-r=|--release=|v)/gi, "");
+      if (versionClear === "7.0.0") {
+        versionClear = "6.1.0";
+      }
+      if (versionClear === "0.0.1") {
+        versionClear = "5.3.0";
+      }
+      expect(
+        cssContent.indexOf(`HTML5 Boilerplate v${versionClear}`) > -1
+      ).toBe(true);
+    });
+  }
+
+  test("./out directory exists", async () => {
+    const outDirExists = await fs.exists(versionFolder(version));
+    expect(outDirExists).toBe(true);
+  });
+
+  test("./out directory have files", async () => {
+    const dirContents = await fs.readdir(versionFolder(version));
+    expect(dirContents.length).toBeGreaterThanOrEqual(5);
+  });
+
+  test("./out directory specific files", async () => {
+    const dirContents = await fs.readdir(versionFolder(version));
+    const check = [
+      "index.html",
+      "robots.txt",
+      "tile.png",
+      "css",
+      "js",
+      "img",
+    ].filter((v) => dirContents.indexOf(v) === -1);
+    expect(check.length === 0).toBe(true);
+  });
+
+  test("Temp dir removed", async () => {
+    const tempDirExists = await fs.exists(tempDir);
+    expect(tempDirExists).toBe(false);
+  });
+});
+
+describe("Errors", () => {
+  test("wrong version 6..2.3", async () => {
+    const mockExit = jest.spyOn(process, "exit").mockImplementation(() => {});
+    const version = "-r=6..2.3";
+
+    await runCli(version);
+
+    await fs.remove(versionFolder(version));
+    expect(mockExit).toHaveBeenCalledWith(1);
+    mockExit.mockRestore();
+  });
+});
