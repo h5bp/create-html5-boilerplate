36ecd3db898e30e4b34d64d6ee50e7dd30f67214
Rob Larsen <rob@htmlcssjavascript.com>
2022-07-03 20:27:03 +0000

    tests run, but all fail

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,8 @@
-export default { transform: {} };
+export default {
+  transform: {},
+  moduleNameMapper: {
+    chalk: "chalk/source/index.js",
+    "#ansi-styles": "chalk/source/vendor/ansi-styles/index.js",
+    "#supports-color": "chalk/source/vendor/supports-color/index.js",
+  },
+};
